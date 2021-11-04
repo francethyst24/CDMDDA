7b8e50ab4c9b5c74674b1fe209a460d016332486
Annabelgwapa <lebannalindo@gmail.com>
2021-11-04 20:10:17 +0800

    bell yern?

--- app/src/main/java/com/example/cdmdda/MainActivity.kt
+++ app/src/main/java/com/example/cdmdda/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+//bell was here
         // region -- ToolBar
         val toolbarMain = findViewById<View>(R.id.toolbarMain) as Toolbar
         setSupportActionBar(toolbarMain)
